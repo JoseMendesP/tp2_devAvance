b0cef060ef47717f5964a35f62003a36afe0f8c2
Jose Mendes <josemendesper24@gmail.com>
2024-02-03 21:02:21 +0100

    Fix issue with image thumbnails and update API URL construction

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-
+import fetch from 'node-fetch';
+import crypto from 'crypto';
 
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
@@ -6,8 +7,43 @@
  * @param url l'end-point
  * @return {Promise<json>}
  */
+const publicKey="700f1e9ecc7a4fd78a1a39c77838df24"
+const privateKey="59691e31a54e80f0eb291f130daa9fa33d3f13d9"
+const ts=  Date.now()
+
 export const getData = async (url) => {
-    // A Compléter
+    try {
+        const response = await fetch(url + "?" + new URLSearchParams({
+            apikey: publicKey,
+            hash: await getHash(publicKey, privateKey, ts),
+            ts: ts,
+        }));
+
+        const responseJson = await response.json();
+
+        if (responseJson && responseJson.data && responseJson.data.results) {
+            const responseResults = responseJson.data.results;
+            let responseWithThumbnail = [];
+
+            responseResults.forEach((element) => {
+                if (element.thumbnail.path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available') {
+                    responseWithThumbnail.push(element);
+                }
+            });
+
+            return responseWithThumbnail.map((character) => {
+                const newCharacter = { ...character };
+                newCharacter.imageUrl = character.thumbnail.path + "/portrait_xlarge." + character.thumbnail.extension;
+                return newCharacter;
+            });
+        } else {
+            console.error('Aucun résultat à été trouvé.');
+            return [];
+        }
+    } catch (error) {
+        console.error('Erreur de récupération:', error);
+        throw error;
+    }
 }
 
 /**
@@ -19,5 +55,7 @@ export const getData = async (url) => {
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
 export const getHash = async (publicKey, privateKey, timestamp) => {
-    // A compléter
+    const hash=crypto.createHash('md5').update(timestamp + privateKey + publicKey).digest("hex")
+    return hash;
 }
+
--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log("Je suis Marvel")
+
+import {getData} from "./api.js";
+
+
+console.log(await getData(`https://gateway.marvel.com:443/v1/public/characters`));
+
+/*
+const fastify = require('fastify')();
+fastify.register(require('@fastify/view'), {
+    engine: {
+        handlebars: require('handlebars')
+    },
+    templates: 'chemin/vers/vos/templates', // Assurez-vous que Fastify peut trouver vos fichiers de templates
+    layout: 'layout.hbs', // Vous pouvez spécifier un layout par défaut si vous le souhaitez
+    partials: {
+        header: 'header.hbs',
+        footer: 'footer.hbs'
+    }
+});
+*/
