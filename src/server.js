abafa2f49e0f6fb50ae6de6c425086aa97ff92c7
Jose Mendes <josemendesper24@gmail.com>
2024-02-04 01:27:10 +0100

    Ajout de l'étape 4 : Intégration de Fastify et Handlebars pour l'affichage des personnages

--- src/server.js
+++ src/server.js
@@ -1,20 +1,41 @@
-
 import {getData} from "./api.js";
+import Fastify from 'fastify';
+import handlebars from 'handlebars';
+import fastifyView from '@fastify/view';
+
+console.log(await getData('https://gateway.marvel.com:443/v1/public/characters'));
 
 
-console.log(await getData(`https://gateway.marvel.com:443/v1/public/characters`));
+const fastify = Fastify({logger: true})
 
-/*
-const fastify = require('fastify')();
-fastify.register(require('@fastify/view'), {
+fastify.register(fastifyView, {
     engine: {
-        handlebars: require('handlebars')
+        handlebars,
     },
-    templates: 'chemin/vers/vos/templates', // Assurez-vous que Fastify peut trouver vos fichiers de templates
-    layout: 'layout.hbs', // Vous pouvez spécifier un layout par défaut si vous le souhaitez
+    includeViewExtension: true,
+    templates: 'templates',
+    layout: 'layout.hbs',
     partials: {
         header: 'header.hbs',
-        footer: 'footer.hbs'
+        footer: 'footer.hbs',
     }
 });
-*/
+fastify.get('/', async function handler(request, reply) {
+    try {
+        const marvelCharacters = await getData("https://gateway.marvel.com:443/v1/public/characters");
+        return reply.view('index.hbs', { marvelCharacters });
+    } catch (error) {
+        fastify.log.error(error);
+        reply.code(500).send('Erreur interne du serveur');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+try {
+    await fastify.listen(PORT, '0.0.0.0');
+    console.log(`Serveur démarré sur le port ${PORT}`);
+} catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+}
